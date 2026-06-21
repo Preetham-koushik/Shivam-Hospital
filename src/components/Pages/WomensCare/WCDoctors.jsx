9d80d11525f328efacf36e2a31c33f2909b4d963
Preetham-koushik <69887483+Preetham-koushik@users.noreply.github.com>
2026-06-21 23:49:33 +0530

    version1.0.6

--- src/components/Pages/WomensCare/WCDoctors.jsx
+++ src/components/Pages/WomensCare/WCDoctors.jsx
@@ -29,7 +29,7 @@ export default function WCDoctors() {
 
   return (
     <section className={`${styles.wrapper} margin`}>
-      <Container className={styles.overflow}>
+      <Container width>
         <div className={styles.title_wrap}>
           <SectionTitle
             title="Meet Our"
